774e8a31ccc2700c28f92e6fdbdda0363e3ebb68
Julian Vazquez <julianvazquez101@gmail.com>
2020-01-23 21:23:44 -0500

    Fix capitalization

--- src/components/top/Navbar.js
+++ src/components/top/Navbar.js
@@ -53,7 +53,7 @@ const Navbar = () => {
             target='_blank'
             rel='noopener noreferrer'
           >
-            <ListItem>Github</ListItem>
+            <ListItem>GitHub</ListItem>
           </a>
         </LinkList>
       </Container>
