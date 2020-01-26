75ed65ce595b9b2b24edc773ef9a5717749cfff2
Julian Vazquez <julianvazquez101@gmail.com>
2020-01-26 01:53:27 -0500

    Make styled-components reusable

--- src/components/projects/PGFinderPage.js
+++ src/components/projects/PGFinderPage.js
@@ -1,146 +1,26 @@
 import React, { Fragment } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
-import styled from 'styled-components';
 import PGFinderDemo from '../../gifs/pg-finder-demo.mp4';
 import PGFinderResponsive from '../../images/pg-finder-responsive.png';
 import PGFinderDocumentation from '../../images/pg-finder-documentation1.png';
+import {
+  Container,
+  InnerContainer,
+  Title,
+  ProjectDescription,
+  SubHeading,
+  StyledParagraph,
+  TechList,
+  ListItem,
+  ButtonContainer,
+  Button,
+  MarginButton,
+  StyledSpan,
+  BackgroundWrapper,
+  PaddingWrapper
+} from '../styled-components/StyledComponents';
 
 const PGFinderPage = () => {
-  const Container = styled.div`
-    max-width: 1400px;
-    width: 90%;
-    display: flex;
-    align-items: center;
-    margin: auto;
-    flex-flow: row wrap;
-    padding-bottom: 4em;
-  `;
-
-  const InnerContainer = styled.div`
-    margin: auto;
-    width: 700px;
-  `;
-
-  const Title = styled.h1`
-    margin-bottom: 1em;
-    letter-spacing: 2px;
-    text-transform: uppercase;
-    font-size: 1.5rem;
-    font-weight: 300;
-  `;
-
-  const ProjectDescription = styled.h2`
-    letter-spacing: -1.5px;
-    line-height: 1.7;
-    font-size: 2.1rem;
-    font-weight: 400;
-    margin-bottom: 1.5em;
-
-    @media (max-width: 700px) {
-      font-size: 1.5rem;
-    }
-  `;
-
-  const SubHeading = styled.h3`
-    margin-bottom: 2em;
-    letter-spacing: 2px;
-    text-transform: uppercase;
-    font-size: 2rem;
-    font-weight: 400;
-    color: ${props => props.theme.gray};
-    position: relative;
-
-    &:after {
-      content: '';
-      position: absolute;
-      bottom: -80%;
-      left: 0;
-      width: 20%;
-      height: 3px;
-      background: ${props => props.theme.primary};
-    }
-
-    @media (max-width: 750px) {
-      font-size: 1.5rem;
-    }
-  `;
-
-  const StyledParagraph = styled.p`
-    font-size: 1.5rem;
-    line-height: 1.7;
-
-    @media (max-width: 700px) {
-      font-size: 1.2rem;
-    }
-  `;
-
-  const TechList = styled.ul`
-    list-style-type: none;
-    list-style-position: inside;
-    font-size: 1.5rem;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-gap: 1em;
-    border-radius: 0.3rem;
-    padding: 2em;
-    box-shadow: 0 5px 15px 0 hsla(0, 0%, 0%, 0.15);
-
-    @media (max-width: 700px) {
-      font-size: 1.2rem;
-    }
-  `;
-
-  const ListItem = styled.li`
-    padding-bottom: 0.5em;
-    border-bottom: 1px solid ${props => props.theme.primary};
-    line-height: 1.7;
-  `;
-
-  const ButtonContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-  `;
-
-  const Button = styled.a`
-    display: inline-block;
-    letter-spacing: 2px;
-    text-transform: uppercase;
-    font-size: 1.5rem;
-    font-weight: 400;
-    background: ${props => props.theme.primary};
-    color: #fff;
-    padding: 0.5em 0.8em;
-    font-family: inherit;
-    border-radius: 0.3rem;
-    cursor: pointer;
-    margin-right: 1em;
-    box-shadow: 0 1px 3px hsla(0, 0%, 0%, 0.2);
-
-    @media (max-width: 600px) {
-      font-size: 1.1rem;
-    }
-
-    @media (max-width: 460px) {
-      margin-bottom: 1em;
-    }
-  `;
-
-  const MarginButton = styled(Button)`
-    margin-top: 4em;
-  `;
-
-  const StyledSpan = styled.span`
-    font-weight: 300;
-  `;
-
-  const BackgroundWrapper = styled.div`
-    background: ${props => props.theme.secondary};
-  `;
-
-  const PaddingWrapper = styled.div`
-    padding: 4em 0;
-  `;
-
   return (
     <Fragment>
       <BackgroundWrapper>
