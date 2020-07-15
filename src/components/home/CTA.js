7d90e88a1c7f5323497f93953b42a07a791c30fe
Julian Vazquez <julianvazquez101@gmail.com>
2020-07-15 15:36:32 -0400

    Add DownArrow icon

--- src/components/home/CTA.js
+++ src/components/home/CTA.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button } from '../styled-components/StyledComponents';
+import { IoIosArrowDown } from 'react-icons/io';
 
 const CTA = () => {
   const CTAContainer = styled.div`
@@ -20,6 +21,9 @@ const CTA = () => {
   `;
 
   const CTA = styled(Button)`
+    display: flex;
+    align-items: center;
+
     @media (max-width: 800px) {
       margin-bottom: 2em;
     }
@@ -28,9 +32,21 @@ const CTA = () => {
     }
   `;
 
+  const DownArrow = styled(IoIosArrowDown)`
+    margin-left: 0.5rem;
+    margin-top: 0.125rem;
+    font-size: 1.75rem;
+
+    @media (max-width: 600px) {
+      font-size: 1.5rem;
+    }
+  `;
+
   return (
     <CTAContainer>
-      <CTA href='#projects'>My projects</CTA>
+      <CTA href='#projects'>
+        My projects <DownArrow />
+      </CTA>
     </CTAContainer>
   );
 };
